f86fe7d7b09dba19e445ade5568721332819f9ce
Michael McEniry [UAH] <mcenirm@uah.edu>
2017-01-31 21:31:31 -0600

    Add wrapper for strace

--- build.sh
+++ build.sh
@@ -12,3 +12,28 @@ strace=${repo_prefix}-strace:latest
 docker run --rm -it -v "${PWD}:/home/blank/src" "${compiler}" make CFLAGS=-pthread pthread1
 
 ( cd docker-strace && docker build -t "${strace}" . )
+
+cat > wrapper-strace.sh <<EOF
+#!/bin/bash
+
+set -e
+set -u
+
+parent=\$( cd "\$( dirname "\$0" )" && /bin/pwd )
+child=\$( /bin/pwd )
+child=\${child#\${parent}}
+workdir=/home/blank/work\${child}
+cmd=(
+    docker
+    run
+    --rm
+    -it
+    --security-opt seccomp:unconfined
+    -v "\${parent}:\${workdir}"
+    -w "\${workdir}"
+    ${strace}
+)
+
+"\${cmd[@]}" "\$@"
+EOF
+chmod +x wrapper-strace.sh
