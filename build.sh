1b39976ac2e25fbc07d3604b8eb62c55e83ea718
Michael McEniry [UAH] <mcenirm@uah.edu>
2017-01-31 21:18:56 -0600

    Build strace image

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ set -u
 
 repo_prefix=$( docker info | sed -n -e 's/^Username: \(.*\)$/\1\//p' | head -1 )ptracetest
 compiler=${repo_prefix}-compiler:latest
+strace=${repo_prefix}-strace:latest
 
 ( cd docker-compiler && docker build -t "${compiler}" . )
 
 docker run --rm -it -v "${PWD}:/home/blank/src" "${compiler}" make CFLAGS=-pthread pthread1
+
+( cd docker-strace && docker build -t "${strace}" . )
